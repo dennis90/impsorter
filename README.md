e41f4d563afd90dd650b68ad11c98193845f02bd
erhise <erikhidrup@gmail.com>
2018-12-22 01:29:28 +0100

    refactor: break out into more structure logic

--- README.md
+++ README.md
@@ -5,13 +5,25 @@ You might think the structure of your imports are important (*no pun intended*).
 ## Features
 
 ### How to use
-`vs-impsorter-extension` supports a number of commands for sorting import statements; these are acessible via the command menu `(Ctrl+Shift+P)` and may be bound to keyboard shortcuts in the normal way.
+`vs-impsorter-extension` supports a number of commands for sorting import statements; these are acessible via the command menu `(<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>)` and may be bound to keyboard shortcuts in the normal way.
 
 ### Sort imports on one line selection
 ![sortLine](https://raw.githubusercontent.com/erhise/impsorter/master/assets/vs-impsorter-sortLine.gif)
 
 ### Sort imports on regard of maximum line width
 ![sortLines](https://raw.githubusercontent.com/erhise/impsorter/master/assets/vs-importer-sortLinesWidth.gif)
+
+## Available commands
+* "Sort Imports: Sort selected line"
+* "Sort Imports: N number of import bindings per line"
+* "Sort Imports: Not exceeding maximum line length"
+
+* Auto sort
+    * Define settings for prefered settings, one-line-sort, max-width, prefer imports on one line each, prefer trailing-comma (true/false)
+    * Sort one line, alphabetical (additional define prefer ordering)
+    * Sort on max-width
+    * Sort on max-imports-per-line
+    * [Bonus] Sort regular array/object alphabetical
 
 ## Extension Settings
 
--- src/import-sorter.ts
+++ src/import-sorter.ts
@@ -1,158 +1,145 @@
-import { window, TextLine, Range } from 'vscode';
+import { Range, Selection, TextEditor, TextLine, window } from 'vscode';
+
+enum SortConfig {
+    SingleLine,
+    MultiLine
+};
 
 export class ImportSorter {
 
-    public sortImportLineCommand(): void {
-        return this.sortImportLine();
-    }
-
-    public sortImportLinesOnWordGroupingCommand(): void {
-        return this.handleSelection(false);
-    }
-
-    public sortImportLinesOnMaxCharWidthCommand(): void {
-        return this.handleSelection(true);
-    }
-
-    private handleSelection(checkForWidth: boolean) {
-        if (!window.activeTextEditor) {
-            return;
-        }
-
-        const editor = window.activeTextEditor;
-        const selection = editor.selection;
-        const selectionEndLine = editor.document.lineAt(selection.end.line);
-
-        const range = new Range(selection.start.line, 0, selection.end.line, selectionEndLine.range.end.character);
-        const input = editor.document.getText(range);
-
-        const result = this.sortImport(input, checkForWidth);
-
-        editor.edit(builder => {
-            builder.replace(range, result);
-        });
-
-        window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
-    }
-
-    private sortImport(inputSelection: any, checkForWidth = false): string {
-        const regex = /\{[^.]*?\}/gm;
-
-        function normalize(input: any) {
-            const removedChars = input.replace(/(\r\n\t|\n|\r\t|\{|\})/gm,"");
-            const splitGroup = removedChars.split(',').map((item: any) => item.trim());
-            splitGroup.sort((a: string, b: string) => {
-                return a.toLowerCase().localeCompare(b.toLowerCase());
-            })
-            return splitGroup;
-        }
-
-        function formatOnWidth(arr: string[]) {
-            const width = checkForWidth;
-            const tabWidth = 4;
-            let string = '{\n' + ' '.repeat(tabWidth);
-            let latestLength = 0;
-            for (let i=0; i < arr.length; i++) {
-              if(i === arr.length - 1) {
-                string += arr[i] + ' \n}';
-              } else {
-                string += arr[i] + ', ';
-              }
-              if (width) {
-                if (string.length - latestLength > 100 && i !== arr.length - 1) {
-                  string += '\n' + ' '.repeat(tabWidth);
-                  latestLength = string.length;
-                }
-              } else {
-                if ((i+1) % 4 === 0 && i !== arr.length - 1) {
-                  string += '\n' + ' '.repeat(tabWidth);
-                }
-              }
-          
+    public sortImportLine(): void {
+        const activeEditor = this.checkEditorAndSelection(
+            window.activeTextEditor, (error: string) => {
+                window.showErrorMessage(error);
+                return;
             }
-            return string;
-        }
+        );
 
-        function sortOnSize(arr: string[]) {
-            if (arr.length > 7) {
-                return formatOnWidth(arr);
-            } else {
-                return `{ ${arr.join(', ')} }`;
-            }
-        }
-          
-        const returnObject = inputSelection.replace(regex, function(x: any) {
-            const normalizedGroup = normalize(x);
-            const formattedGroup = sortOnSize(normalizedGroup);
-            return formattedGroup;
-        });
+        if (activeEditor === undefined) { return; }
 
-        return returnObject;
-    }
+        const [editor, selection] = activeEditor;
 
-    private sortImportLine(): void {
-        if (!window.activeTextEditor) {
-            return;
-        }
-
-        const editor = window.activeTextEditor;
-        const selection = editor.selection;
-
-        // check if more than one line is selected
-        if (selection.start.line !== selection.end.line) {
+        if (isMultiline(selection)) {
             window.showErrorMessage(`More than one line was selected`);
             return;
         }
 
-        // just to test functionality out
         const lineToSort = editor.document.lineAt(selection.start.line);
 
-        // check if line contains import
-        const isImport = lineToSort.text.includes('import');
-        if (!isImport) {
+        if (!isImportStatement(lineToSort)) {
             window.showErrorMessage(`Selected line does not seem to have an import statement`);
             return;
         }
 
-        const sortedLine = this.sortImportLineObject(lineToSort);
-        editor.edit(builder => {
-            builder.replace(lineToSort.range, sortedLine);
-        });
-
+        const sortedLine = sortImportSelection(lineToSort.text, SortConfig.SingleLine);
+        editor.edit(builder => builder.replace(lineToSort.range, sortedLine));
         window.showInformationMessage(`Line: ${selection.start.line + 1} got sorted.`);
     }
 
-    private sortImportLineObject(inputTextLine: TextLine) {
 
-        // destruct from TextLine object
-        const inputLine = inputTextLine.text;
+    public sortImportLinesOnWordGroupingCommand(): void {
+        const activeEditor = this.checkEditorAndSelection(
+            window.activeTextEditor, (error: string) => {
+                window.showErrorMessage(error);
+                return;
+            }
+        );
 
-        // find everything between curly brackets
-        const regex = /\{(.*?)\}/g;
+        if (activeEditor !== undefined) {
+            const [editor, selection] = activeEditor;
+            const selectionEndLine = editor.document.lineAt(selection.end.line);
 
-        // execute regexp to get matches
-        const result = regex.exec(inputLine);
+            const range = new Range(selection.start.line, 0, selection.end.line, selectionEndLine.range.end.character);
+            const input = editor.document.getText(range);
 
-        // 1st capturing group excluding curly brackets
-        const firstCapturingGroup = result ? result[1] : '';
-
-        // string to array
-        const groupToSort = firstCapturingGroup.split(',').map(item => item.trim());
-
-        // avoid mutating
-        const sortedGroup = groupToSort.slice(0);
-        sortedGroup.sort((a, b) => {
-            return a.toLowerCase().localeCompare(b.toLowerCase());
-        });
-
-        // add curly brackets
-        const sortedLine = `{ ${sortedGroup.join(', ')} }`;
-
-        return inputLine.replace(regex, sortedLine);
+            const sortedImport = sortImportSelection(input, SortConfig.MultiLine);
+            editor.edit(builder => builder.replace(range, sortedImport));
+            window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
+        }
     }
 
 
-    public dispose() {
+    public sortImportLinesOnMaxCharWidthCommand(): void {
+        const activeEditor = this.checkEditorAndSelection(
+            window.activeTextEditor, (error: string) => {
+                window.showErrorMessage(error);
+                return;
+            }
+        );
 
+        if (activeEditor !== undefined) {
+            const [editor, selection] = activeEditor;
+            const selectionEndLine = editor.document.lineAt(selection.end.line);
+
+            const range = new Range(selection.start.line, 0, selection.end.line, selectionEndLine.range.end.character);
+            const input = editor.document.getText(range);
+
+            const sortedImport = sortImportSelection(input, SortConfig.MultiLine);
+            editor.edit(builder => builder.replace(range, sortedImport));
+            window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
+        }
+    }
+
+
+    // Make sure editor is active and that there is a selection.
+    private checkEditorAndSelection(editor: TextEditor | undefined, cb: (err: string) => void): [TextEditor, Selection] | void {
+        if (editor === undefined) { return cb(`Editor is not active`); }
+        if (editor.selection === undefined) { return cb(`No valid selection`); }
+        return [ editor, editor.selection ];
+    }
+
+}
+
+
+// ---------- HELP FUNCTIONS ------------------------------------------
+function isMultiline(selection: Selection): boolean {
+    return selection.start.line !== selection.end.line;
+}
+
+function isImportStatement(selection: TextLine): boolean {
+    return selection.text.includes('import');
+}
+
+function sortImportSelection(selection: string, format: SortConfig): string {
+    return selection.replace(/\{[^.]*?\}/gm, exp => {
+        const normalized = exp.replace(/\s/g, "");
+        const match = RegExp(/\{(.*?)\}/,'g').exec(normalized);
+        if (match === null || match[1] === undefined) {
+            return exp;
+        }
+        const arrayToSort = match[1].split(',');
+        const sortedArray = sortArray(arrayToSort);
+        return formatArray(sortedArray, format);
+    });
+}
+
+function sortArray(arr: string[]): string[] {
+    return arr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
+
+function formatArray(arr: string[], format: SortConfig): string {
+    let formattedArray;
+    switch (format) {
+        case SortConfig.SingleLine: {
+            formattedArray = arr.map((entry, index) => {
+                if (index !== 0) {
+                    entry = ' ' + entry;
+                }
+                return entry;
+            });
+            return `{ ${formattedArray} }`;
+        }
+
+        case SortConfig.MultiLine: {
+            formattedArray = arr.map((entry, index) => {
+                entry = '\n    ' + entry;
+                return entry;
+            });
+            return `{${formattedArray}\n}`;
+        }
+    
+        default: {
+            return arr.toString();
+        }
     }
 }
